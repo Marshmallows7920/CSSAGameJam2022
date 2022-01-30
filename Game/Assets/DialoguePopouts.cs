895525061e003b373755cbff1ff1eddf069aac80
Kha Pham <Khathepham@gmail.com>
2022-01-30 17:31:07 -0600

    Last push

--- Game/Assets/DialoguePopouts.cs
+++ Game/Assets/DialoguePopouts.cs
@@ -5,17 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class DialoguePopouts : MonoBehaviour
 {
+    bool pause = false;
+    bool currentStatus = false;
+
+    [SerializeField] GameObject pauseMenu;
 
     void Start()
-{
-    Debug.Log("LoadSceneB");
-}
+    {
+        Debug.Log("LoadSceneB");
+    }
 
-public void LoadB(int sceneANumber)
-{
-    Debug.Log("sceneBuildIndex to load: " + sceneANumber);
-    SceneManager.LoadScene(sceneANumber);
-}
+    void update() {
+        
+    }
+
+    public void LoadB(int sceneANumber)
+    {
+        Debug.Log("sceneBuildIndex to load: " + sceneANumber);
+        SceneManager.LoadScene(sceneANumber);
+    }
+
+    private void pauseEnable()
+    {
+        if (!currentStatus && pause)
+        {
+            pauseMenu.SetActive(true);
+        }
+        else if (currentStatus && !pause) {
+            pauseMenu.SetActive(false);
+        }
+    }
+
 
 
 }
