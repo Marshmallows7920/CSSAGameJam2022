a00aa2573349c1f2d008565e4d39fefc7ca9e175
sarah <sarahvel4@gmail.com>
2022-01-30 11:56:30 -0600

    All buttons functioning

--- Game/Assets/MenuScreen.cs
+++ Game/Assets/MenuScreen.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuScreen : MonoBehaviour
 {
-
-public void PlayGame()
+    void Start()
 {
-    SceneManager.LoadScene("Background");
+    Debug.Log("LoadSceneB");
 }
 
+public void LoadB(int sceneANumber)
+{
+    Debug.Log("sceneBuildIndex to load: " + sceneANumber);
+    SceneManager.LoadScene(sceneANumber);
+}
+
+public void QuitScreen()
+{
+    Application.Quit();
+}
 }
