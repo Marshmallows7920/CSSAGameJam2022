b60acf743c2daeb99ea2078329b3b57cd738d5a1
sarah <sarahvel4@gmail.com>
2022-01-30 11:33:02 -0600

    Controls button functioning

---
+++ Game/Assets/MenuScreen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScreen : MonoBehaviour
+{
+
+public void PlayGame()
+{
+    SceneManager.LoadScene("Background");
+}
+
+}
